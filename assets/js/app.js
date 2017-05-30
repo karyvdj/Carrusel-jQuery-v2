ebbe79e2badf57adca3b825403f9e59b25f591b2
karyvdj <kaina51@hotmail.com>
2017-05-30 14:47:39 -0500

    Funcionalidad en jquery de carrusel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,45 @@
+// $(document).ready(function(){
+// 	alert("soy jQuery");
+// });
+
 var target = 0;
 
 var cargarPagina = function () {
 	// Elementos
-	var botones = document.querySelectorAll(".control");
-	var anterior = document.querySelector(".previous");
-	var siguiente = document.querySelector(".next");
+	var $botones = $(".control");
+	var $anterior = $(".previous");
+	var $siguiente = $(".next");
 
 	// Eventos
-	botones.forEach(function (boton) {
-		boton.addEventListener("click", cambiarImagen);
-	});
-	anterior.addEventListener("click", anteriorImagen);
-	siguiente.addEventListener("click", siguienteImagen);
+	$botones.click(cambiarImagen);
+	$anterior.click(anteriorImagen);
+	$siguiente.click(siguienteImagen);
 };
 
 var cambiarImagen = function () {
-	target = parseInt(this.dataset.target);
+	target = parseInt($(this).data("target"));
 	mostrarImagen(target);
 };
 
 var mostrarImagen = function (target) {
-	var lastSlide = document.querySelector("div.active");
-	var slide = document.querySelector("div[data-slide='" + target + "']");
-	lastSlide.classList.remove("active");
-	slide.classList.add("active");
+	var $lastSlide = $("div.active");
+	var $slide = $("div[data-slide='" + target + "']");
+	$lastSlide.removeClass("active");
+	$slide.addClass("active");
 };
 
 var anteriorImagen = function (e) {
 	e.preventDefault();
 	target = target - 1;
-	target = (target < 0) ? 3 : target;
+	target = (target < 0) ? 4 : target;
 	mostrarImagen(target);
 };
 
 var siguienteImagen = function (e) {
 	e.preventDefault();
 	target = target + 1;
-	target = (target > 3) ? 0 : target;
+	target = (target > 4) ? 0 : target;
 	mostrarImagen(target);mostrarImagen(target);
 };
 
-
-
-window.addEventListener("load", cargarPagina);
+$(document).ready(cargarPagina);
